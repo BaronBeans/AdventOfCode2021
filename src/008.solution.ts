12a6a8c8d1d1009eac4830890f04fa53836adcbe
Sean Barnard <sean.barnard.94@hotmail.com>
2021-12-08 22:05:50 +0000

    .

--- src/008.solution.ts
+++ src/008.solution.ts
@@ -91,38 +91,91 @@ const getLikelyNumber = (size: number, answers: {}, digit: string) => {
   }
 };
 
+// export const Solve8x2 = async () => {
+//   const input = await GetTestData();
+//   const lines = input.trim().split("\n");
+
+//   const outputValues = lines.map((x) => x.split(" | "));
+
+//   const formatted = outputValues.map((line) => {
+//     let answers: Record<string, number> = {};
+//     return line[0].split(" ").map((digit) => {
+//       const size = new Set(Array.from(digit.split(""))).size;
+//       const guess: string | number = getLikelyNumber(size, answers, digit);
+//       Object.assign(answers, {
+//         [digit]: guess,
+//       });
+//       console.log("digit", digit);
+//       // console.log(
+//       //   "answers",
+//       //   Object.entries(answers)
+//       //     .map(([key, value]) => ({ key, value }))
+//       //     .filter((x) =>
+//       //       x.value
+//       //         .toString()
+//       //         .split("")
+//       //         .map((x) => )
+//       //     )
+//       // );
+//       return {
+//         digit,
+//         guess,
+//       };
+//     });
+//   });
+
+//   console.log(`Day 8 Part 2 - `);
+// };
+
+type Segment = "a" | "b" | "c" | "d" | "e" | "f" | "g";
+
+type Map = Record<Segment, Segment>;
+
+const workOutDigits = (map: string) => {
+  let mapArray = new Array(10).fill(0);
+  const digits = map.split(" ").sort((a, b) => a.length - b.length);
+  digits.forEach((d) => {
+    if (d.length === 2) {
+      mapArray[0] = d;
+    }
+    if (d.length === 3) {
+      mapArray[2] = d;
+    }
+    if (d.length === 4) {
+      mapArray[3] = d;
+    }
+    if (d.length === 7) {
+      mapArray[7] = d;
+    }
+  });
+  const remainingDigits = digits.filter((x) => !mapArray.includes(x));
+
+  const twosEtc = digits.filter((x) => x.length === 5);
+  const zerosEtc = digits.filter((x) => x.length === 6);
+
+  console.log(mapArray);
+  console.log(remainingDigits);
+  console.log(twosEtc.filter((x) => x)); // need to work out which is unique/has a segment from number 8 mapping
+  console.log(zerosEtc);
+};
+
 export const Solve8x2 = async () => {
   const input = await GetTestData();
   const lines = input.trim().split("\n");
 
-  const outputValues = lines.map((x) => x.split(" | "));
+  // const splits = lines.slice(0).map((line) => {
+  //   const [map, code] = line.split(" | ");
+  //   console.log("map", map);
+  //   console.log("code", code);
 
-  const formatted = outputValues.map((line) => {
-    let answers: Record<string, number> = {};
-    return line[0].split(" ").map((digit) => {
-      const size = new Set(Array.from(digit.split(""))).size;
-      const guess: string | number = getLikelyNumber(size, answers, digit);
-      Object.assign(answers, {
-        [digit]: guess,
-      });
-      console.log("digit", digit);
-      console.log(
-        "answers",
-        Object.entries(answers)
-          .map(([key, value]) => ({ key, value }))
-          .filter((x) =>
-            x.value
-              .toString()
-              .split("")
-              .map((x) => )
-          )
-      );
-      return {
-        digit,
-        guess,
-      };
-    });
-  });
+  //   return { map, code };
+  // });
+  // console.log(splits[0]);
 
-  console.log(`Day 8 Part 2 - `);
+  const [map, code] = lines[0].split(" | ");
+
+  console.log("map", map);
+  console.log("code", code);
+
+  workOutDigits(map);
 };
