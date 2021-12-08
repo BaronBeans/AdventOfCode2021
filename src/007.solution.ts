e28a2c05727519642a160bb03e2d4643ee63cb7d
Sean Barnard <sean.barnard.94@hotmail.com>
2021-12-08 15:53:12 +0000

    Day 7 complete

--- src/007.solution.ts
+++ src/007.solution.ts
@@ -11,15 +11,46 @@ const GetTestData = async () => {
 };
 
 export const Solve7x1 = async () => {
-  const input = await GetTestData();
+  const input = await GetInputData();
   const splitInput = input.trim().split(",");
-  console.log(splitInput);
-  console.log(`Day 7 Part 1 - `);
+
+  const poitions = splitInput.map(Number);
+  const totals = [];
+
+  for (let i = Math.min(...poitions); i <= Math.max(...poitions); i++) {
+    const movements = poitions.map((p) => Math.abs(p - i));
+    const total = movements.reduce((acc, cur) => acc + cur, 0);
+    totals.push(total);
+  }
+
+  const answer = Math.min(...totals);
+  console.log(`Day 7 Part 1 - ${answer}`);
+};
+
+const getNthTriangleNumber = (i: number): number => {
+  if (i === 0) {
+    return 0;
+  }
+  return i + getNthTriangleNumber(i - 1);
 };
 
 export const Solve7x2 = async () => {
-  const input = await GetTestData();
+  const input = await GetInputData();
   const splitInput = input.trim().split(",");
 
-  console.log(`Day 7 Part 2 - `);
+  const poitions = splitInput.map(Number);
+  const totals = [];
+
+  for (let i = Math.min(...poitions); i <= Math.max(...poitions); i++) {
+    const movements = poitions.map((p) => Math.abs(p - i));
+    const total = movements.reduce(
+      (acc, cur) => acc + getNthTriangleNumber(cur),
+      0
+    );
+    totals.push(total);
+  }
+
+  const answer = Math.min(...totals);
+
+  console.log(`Day 7 Part 2 - ${answer}`);
 };
