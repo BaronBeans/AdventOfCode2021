8afe2d82e0e840ecf9215a544057ef48020a2c3e
Sean Barnard <sean.barnard.94@hotmail.com>
2021-12-10 12:41:20 +0000

    Day 9 Complete

--- src/009.solution.ts
+++ src/009.solution.ts
@@ -10,98 +10,102 @@ const GetTestData = async () => {
   return Buffer.toString();
 };
 
+type Point = {
+  x: number;
+  y: number;
+};
+
+const getNeighbourPositions = (x: number, y: number, data: string[][]) => {
+  const neighbours: Point[] = [];
+  if (y > 0) {
+    neighbours.push({
+      x,
+      y: y - 1,
+    });
+  }
+  if (y < data.length - 1) {
+    neighbours.push({
+      x,
+      y: y + 1,
+    });
+  }
+  if (x > 0) {
+    neighbours.push({
+      x: x - 1,
+      y,
+    });
+  }
+  if (x < data[y].length - 1) {
+    neighbours.push({
+      x: x + 1,
+      y,
+    });
+  }
+
+  return neighbours;
+};
+
 export const Solve9x1 = async () => {
   const input = await GetInputData();
   const splitInput = input
     .trim()
     .split("\n")
     .map((x) => x.split(""));
-
-  const getNeighbours = (x: number, y: number) => {
-    const neighbours = [];
-    if (y > 0) {
-      neighbours.push(splitInput[y - 1][x]);
-    }
-    if (y < splitInput.length - 1) {
-      neighbours.push(splitInput[y + 1][x]);
-    }
-    if (x > 0) {
-      neighbours.push(splitInput[y][x - 1]);
-    }
-    if (x < splitInput[y].length - 1) {
-      neighbours.push(splitInput[y][x + 1]);
-    }
-
-    return neighbours;
-  };
-
   let count = 0;
-
   for (let y = 0; y < splitInput.length; y++) {
     for (let x = 0; x < splitInput[y].length; x++) {
-      let value = parseInt(splitInput[y][x]);
-      let neighbours = getNeighbours(x, y);
-      if (
-        neighbours.filter((n) => parseInt(n) > value).length ===
-        neighbours.length
-      ) {
-        count += value + 1;
+      let neighbours = getNeighbourPositions(x, y, splitInput);
+      if (neighbours.every((n) => splitInput[n.y][n.x] > splitInput[y][x])) {
+        count += +splitInput[y][x] + 1;
       }
     }
   }
-
   console.log(`Day 9 Part 1 - ${count}`);
 };
 
+const getBasinNeighbours = (
+  x: number,
+  y: number,
+  data: string[][],
+  basin: Point[]
+): Point[] => {
+  let neighbours = getNeighbourPositions(x, y, data);
+
+  neighbours.forEach((n) => {
+    if (+data[n.y][n.x] === 9) {
+      return;
+    }
+    if (basin.filter((b) => b.x === n.x && b.y === n.y).length) {
+      return;
+    }
+    basin.push({ x: n.x, y: n.y });
+    basin = getBasinNeighbours(n.x, n.y, data, basin);
+  });
+
+  return basin;
+};
+
 export const Solve9x2 = async () => {
-  const input = await GetTestData();
+  const input = await GetInputData();
   const splitInput = input
     .trim()
     .split("\n")
     .map((x) => x.split(""));
-
-  const getNeighbours = (x: number, y: number) => {
-    const neighbours = [];
-    if (y > 0) {
-      neighbours.push(splitInput[y - 1][x]);
-    }
-    if (y < splitInput.length - 1) {
-      neighbours.push(splitInput[y + 1][x]);
-    }
-    if (x > 0) {
-      neighbours.push(splitInput[y][x - 1]);
-    }
-    if (x < splitInput[y].length - 1) {
-      neighbours.push(splitInput[y][x + 1]);
-    }
-
-    return neighbours;
-  };
-
-  const lowPoints = [];
-
+  const basinSizes = [];
   for (let y = 0; y < splitInput.length; y++) {
     for (let x = 0; x < splitInput[y].length; x++) {
-      let value = parseInt(splitInput[y][x]);
-      let neighbours = getNeighbours(x, y);
-      if (
-        neighbours.filter((n) => parseInt(n) > value).length ===
-        neighbours.length
-      ) {
-        lowPoints.push({ x, y });
+      let neighbours = getNeighbourPositions(x, y, splitInput);
+      if (neighbours.every((n) => splitInput[n.y][n.x] > splitInput[y][x])) {
+        let basinNeighbours = getBasinNeighbours(x, y, splitInput, []);
+
+        basinSizes.push(basinNeighbours.length);
       }
     }
   }
 
-  //   Want to get the lowest points and then starting with their neighbours get their neighbours and keep going up until you get to 9's
-
-  const values = lowPoints.map((l) => {
-    let value = splitInput[l.y][l.x];
-    return value;
-  });
-
-  console.log(lowPoints);
-  console.log(values);
-
-  console.log(`Day 9 Part 2 - `);
+  const answer = basinSizes
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((acc, cur) => acc * cur, 1);
+  console.log(`Day 9 Part 2 - ${answer}`);
 };
